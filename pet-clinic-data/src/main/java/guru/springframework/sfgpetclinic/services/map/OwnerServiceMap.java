d74cd8c0a3b73e788d230a0e0940575a417410b2
Donald F Coffin <donald.coffin@reminetworks.com>
2018-08-04 17:59:06 -0700

    Implement Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,40 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+/**
+ * Created by REMI Networks
+ * User: don
+ * Date: 8/4/18
+ * Time: 5:45 PM
+ **/
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.findById(id);
+    }
+}
