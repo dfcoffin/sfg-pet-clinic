6fa1cebf4f72de1cc1049f5ada89930db3b56716
Donald F Coffin <donald.coffin@reminetworks.com>
2018-08-05 19:28:09 -0700

    Added PetType Service. Closes #29

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by REMI Networks
+ * User: don
+ * Date: 8/5/18
+ * Time: 7:20 PM
+ **/
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
