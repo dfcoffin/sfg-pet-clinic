6ad56e5c4f058272711b551d51a5c8563c0bf5af
Donald F Coffin <donald.coffin@reminetworks.com>
2018-08-05 21:54:10 -0700

    Added Specialties Map Service Implementation. Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+/**
+ * Created by REMI Networks
+ * User: don
+ * Date: 8/5/18
+ * Time: 8:54 PM
+ **/
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
