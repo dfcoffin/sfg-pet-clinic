4f9c935b779e399f393c1c996bcde9d0c8a20cc9
Donald F Coffin <donald.coffin@reminetworks.com>
2018-08-22 12:46:53 -0700

    Update Vet with JPA entity mappings. Closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by REMI Networks
  * User: don
  * Date: 8/5/18
  * Time: 7:06 PM
  **/
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
