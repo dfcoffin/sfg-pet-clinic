f51f6812a9657eb6773e8678d215e3ce660cd6dd
Donald F Coffin <donald.coffin@reminetworks.com>
2018-08-22 15:34:20 -0700

    Implemented Map implementation for Visit Service. Closes #47.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by REMI Networks
+ * User: don
+ * Date: 8/22/18
+ * Time: 3:07 PM
+ **/
+public interface VisitService extends CrudService<Visit, Long> {
+}
